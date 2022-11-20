54b5b59ebd9f1f68cdd1dbfe87d98785c16d482b
ART89-prog <dykpgilkom@mail.ru>
2022-11-20 22:02:37 +0300

    Добавил новый проект

--- js/scripts.js
+++ js/scripts.js
@@ -73,58 +73,58 @@ $(() => {
     $(document).on('change', '.error', function () {
 
         $(this).removeClass('error');
-        if ($(this).attr('class') != 'checked') { $(this).next().hide(); }
+        /*if ($(this).attr('class') != 'checked') { $(this).next().hide(); }*/
     })
 
-    $(document).on('click', '.submit_btn', function (event) {
-        event.preventDefault();
-        var dataForAjax = "action=form&";
-        var addressForAjax = myajax.url;
+    $(document).on('click', '.js-submit', function (event) {
+        event.preventDefault();        
         var valid = true;
         var form = $(this).closest('form');
         $(this).closest('form').find('input:not([type=submit]),textarea').each(function (i, elem) {
             if (this.value.length < 3 && $(this).hasClass('required')) {
                 valid = false;
                 $(this).addClass('error');
-                $(this).next().show();
             }
             if ($(this).attr('name') == 'email' && $(this).hasClass('required')) {
                 var pattern = /^([a-z0-9_\.-])+@[a-z0-9-]+\.([a-z]{2,4}\.)?[a-z]{2,4}$/i;
                 if (!pattern.test($(this).val())) {
                     valid = false;
                     $(this).addClass('error');
-                    $(this).next().show();
                 }
             }
             if ($(this).hasClass("checked") && !$(this).prop("checked")) {
                 $(this).addClass('error');
                 valid = false;
             }
-
-            if (i > 0) {
-                dataForAjax += '&';
-            }
-            dataForAjax += this.name + '=' + this.value;
         })
 
         if (!valid) {
             return false;
         }
 
+        let form_create = document.querySelector("#form_create");
+
+        var addressForAjax = myajax.url;            
+        var formData = new FormData(form_create);
+        formData.append('action', "form");
+
+        /*formData.append('name', $('#name').val());        
+        formData.append('text', $('#text').val());
+        formData.append('phone', $('#phone').val());
+        formData.append('email', $('#email').val());
+
+        formData.append('file', $('#file')[0].files[0]);*/
+        console.log(formData);
         $.ajax({
             type: 'POST',
-            data: dataForAjax,
+            data: formData,
             url: addressForAjax,
-            success: function (response) {
-
-                Fancybox.close()
-
-                Fancybox.show([{
-                    src: "#thanks",
-                    type: 'inline'
-                }])
-
-                $('form').trigger("reset");
+            cache: false,
+            contentType: false,
+            processData: false,
+            success: function(response) {
+                alert("Принят в обработку!");
+                $('form').trigger("reset");                      
             }
         });
     });
